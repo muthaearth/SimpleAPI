7c93364e9ffa6452e5a986ee2791e4e6baa846ca
cheryl summers <60108714+muthaearth@users.noreply.github.com>
2020-08-01 09:08:22 -0400

    Initial Commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
